6779b73c12304a55a294c3b0c58aff5b4552d41f
jphulse <hulsej281@gmail.com>
2026-03-26 15:46:15 -0400

    lazy to write

--- escape-room/player/player.gd
+++ escape-room/player/player.gd
@@ -8,3 +8,4 @@ const JUMP_VELOCITY = -400.0
 func _physics_process(_delta: float) -> void:
 	velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down") * speed
 	move_and_slide()
+	
--- escape-room/puzzles/puzzle.gd
+++ escape-room/puzzles/puzzle.gd
@@ -3,11 +3,12 @@ class_name Puzzle extends Node
 signal request_close
 signal puzzle_solved
 
+
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		request_close.emit()
-	elif event.is_action_pressed("ui_right"):
-		solve()
+	
 
 func solve() -> void:
 	puzzle_solved.emit()
