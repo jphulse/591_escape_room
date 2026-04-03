17406354afaa4d9b8346163cf2145b1c94c95296
jphulse <hulsej281@gmail.com>
2026-04-03 16:10:20 -0400

    refined simon prototype

--- escape-room/puzzles/microgames/simon/simon.gd
+++ escape-room/puzzles/microgames/simon/simon.gd
@@ -7,6 +7,8 @@ enum Direction {
 @export var cycles : int = 3
 @export var first_cycle_length := 4
 @export var cycle_delta := 1
+@export var sequence_cooldown := 1.0
+@export var cycle_cooldown := 3.0
 
 @export_group("Shader info")
 @export_range(0.0, 1.0) var darkness_mult : float = .5
@@ -18,19 +20,23 @@ enum Direction {
 
 @onready var cycle_timer : Timer = $CycleTimer
 @onready var sequence_timer : Timer = $SequenceTimer
+@onready var sprite_timer : Timer = $SpriteHighlightTimer
 
 
 var sequence : Array[Direction] = []
 var sprite_map : Dictionary[SimonButton, Direction]
 var cycle := 0
+var current_index := 0
 
 func start_round() -> void:
+	current_index = 0
 	sequence.clear()
 	for button : SimonButton in sprite_map.keys():
 		button.selectable = false
 	for i in range(first_cycle_length + cycle * cycle_delta):
 		sequence.append(Direction.values().pick_random())
 	print(sequence)
+	sequence_timer.start(sequence_cooldown)
 
 
 
@@ -53,15 +59,39 @@ func _ready() -> void:
 
 
 func _on_cycle_timer_timeout() -> void:
-	pass # Replace with function body.
+	start_round()
 
 
 func _on_sequence_timer_timeout() -> void:
-	pass # Replace with function body.
-
+	
+	if current_index > 0:
+		var old_sprite : SimonButton = sprite_map.find_key(sequence[current_index - 1])
+		old_sprite.set_instance_shader_value("darkness_mult", old_sprite.darkness_mult)
+	if current_index >= len(sequence):
+		sequence_timer.stop()
+		for sprite : SimonButton in sprite_map.keys():
+			sprite.selectable = true
+		cycle_timer.start(cycle_cooldown)
+		return
+	sprite_timer.start(sequence_cooldown / 2.0)
+	
 
 func _on_button_pressed(caller: SimonButton) -> void:
-	if sequence.front() == sprite_map[caller]:
+	if not sequence.is_empty() and sequence.front() == sprite_map[caller]:
 		sequence.pop_front()
 		if sequence.is_empty():
-			print("Solved")
+			cycle_timer.stop()
+			cycle += 1
+			if cycle >= cycles:
+				puzzle_solved.emit()
+				print("Solved")
+			else:
+				start_round()
+	else:
+		start_round()
+
+
+func _on_sprite_highlight_timer_timeout() -> void:
+	var sprite : SimonButton = sprite_map.find_key(sequence[current_index])
+	sprite.set_instance_shader_value("darkness_mult", sprite.active_mult)
+	current_index += 1
