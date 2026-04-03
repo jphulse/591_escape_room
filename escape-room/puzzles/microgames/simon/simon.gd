21e65eb0948ab87367117d5dd7a55b960614474a
jphulse <hulsej281@gmail.com>
2026-04-03 12:32:39 -0400

    wip simon

--- escape-room/puzzles/microgames/simon/simon.gd
+++ escape-room/puzzles/microgames/simon/simon.gd
@@ -1,5 +1,12 @@
 extends Puzzle
 
+enum Direction {
+	TOP, RIGHT, BOTTOM, LEFT
+}
+@export_group("Simon game info")
+@export var cycles : int = 3
+@export var first_cycle_length := 4
+@export var cycle_delta := 1
 
 @export_group("Shader info")
 @export_range(0.0, 1.0) var darkness_mult : float = .5
@@ -8,6 +15,24 @@ extends Puzzle
 @onready var right_sprite : SimonButton = $RightSprite
 @onready var bottom_sprite : SimonButton = $BottomSprite
 @onready var left_sprite : SimonButton = $LeftSprite
+
+@onready var cycle_timer : Timer = $CycleTimer
+@onready var sequence_timer : Timer = $SequenceTimer
+
+
+var sequence : Array[Direction] = []
+var sprite_map : Dictionary[SimonButton, Direction]
+var cycle := 0
+
+func start_round() -> void:
+	sequence.clear()
+	for button : SimonButton in sprite_map.keys():
+		button.selectable = false
+	for i in range(first_cycle_length + cycle * cycle_delta):
+		sequence.append(Direction.values().pick_random())
+	print(sequence)
+
+
 
 func _ready() -> void:
 	top_sprite.darkness_mult = darkness_mult
@@ -18,5 +43,25 @@ func _ready() -> void:
 	right_sprite.set_instance_shader_value("darkness_mult", darkness_mult)
 	bottom_sprite.set_instance_shader_value("darkness_mult", darkness_mult)
 	left_sprite.set_instance_shader_value("darkness_mult", darkness_mult)
+	sprite_map[top_sprite] = Direction.TOP
+	sprite_map[right_sprite] = Direction.RIGHT
+	sprite_map[bottom_sprite] = Direction.BOTTOM
+	sprite_map[left_sprite] = Direction.LEFT
+	start_round()
 	
 # Called when the node enters the scene tree for the first time.
+
+
+func _on_cycle_timer_timeout() -> void:
+	pass # Replace with function body.
+
+
+func _on_sequence_timer_timeout() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_pressed(caller: SimonButton) -> void:
+	if sequence.front() == sprite_map[caller]:
+		sequence.pop_front()
+		if sequence.is_empty():
+			print("Solved")
--- escape-room/puzzles/microgames/simon/simon_button.gd
+++ escape-room/puzzles/microgames/simon/simon_button.gd
@@ -2,15 +2,19 @@ class_name SimonButton extends Sprite2D
 
 signal pressed(caller : SimonButton)
 
+
+
 @export var active_mult : float = 2.0
 @export var cooldown_tint : float = .5
 @export var cooldown_time := .25
+
 
 var darkness_mult : float = 1.0
 var active : bool = false
 var selectable : bool = true
 var released : bool = false
 var on_cooldown : bool = false
+
 
 @onready var cooldown_timer : Timer = $CooldownTimer
 
@@ -29,7 +33,7 @@ func release() -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and active:
+		if event.button_index == MOUSE_BUTTON_LEFT and active and selectable:
 			if event.pressed:
 				click()
 			else:
@@ -57,5 +61,5 @@ func _on_area_mouse_exited() -> void:
 
 func _on_cooldown_timer_timeout() -> void:
 	on_cooldown = false
-	if released and active:
+	if released and active and selectable:
 		set_instance_shader_value("darkness_mult", active_mult)
