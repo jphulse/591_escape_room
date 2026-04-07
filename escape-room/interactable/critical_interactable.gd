f784bb040e41b79ab9fc97933c78d3f925262f72
Anant Patel <103785977+anantptl@users.noreply.github.com>
2026-04-06 20:24:15 -0400

    Pushing changes to make all my puzzles critical interactables

--- escape-room/interactable/critical_interactable.gd
+++ escape-room/interactable/critical_interactable.gd
@@ -1,9 +1,14 @@
 extends Interactable
 
+var is_solved = false
 signal critical_interactable_solved(caller : Interactable)
-
-
 
 func on_puzzle_solved() -> void:
 	super.on_puzzle_solved()
 	critical_interactable_solved.emit(self)
+	is_solved = true
+
+func _physics_process(_delta: float) -> void:
+	if is_solved:
+		return
+	super._physics_process(_delta)
