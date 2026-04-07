cdaf629ea358be49e298688cad4cacddde5db7c2
Anant Patel <103785977+anantptl@users.noreply.github.com>
2026-04-06 21:24:44 -0400

    Cleaned up player.

--- escape-room/player/player.gd
+++ escape-room/player/player.gd
@@ -2,12 +2,10 @@ class_name Player extends CharacterBody2D
 
 @export var speed  := 300.0
 @export var rotation_speed := 10.0
+const JUMP_VELOCITY = -400.0
+var input_enabled := true
 
 signal vision_cone() 
-
-const JUMP_VELOCITY = -400.0
-
-var input_enabled := true
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down") 
